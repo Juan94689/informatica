6fbcdd55bd95a0e5c6d6af73b8a5f059cc097a1e
Juan94689 <juan.andres.151009@gmail.com>
2025-09-18 15:05:06 -0300

    Add files via upload

---
+++ ejercicio1.js
@@ -0,0 +1,6 @@
+alert("hola mundo");
+let btn=document.getElementById("boton") 
+let parrafo=document.getElementById("texto")
+btn.addEventListener("click"),function(){
+parrafo.innerHTML="Marinellis";
+}
