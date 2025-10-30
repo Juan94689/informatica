31e41fb769d10aef69979cd9bfb7f792e384a161
Juan94689 <juan.andres.151009@gmail.com>
2025-10-30 14:25:15 -0300

    Update ejercicio1.js

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,6 +1,30 @@
-alert("hola mundo");
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    alert("hola mundo");
 let btn=document.getElementById("boton") 
 let parrafo=document.getElementById("texto")
 btn.addEventListener("click"),function(){
 parrafo.innerHTML="Marinellis";
 }
+
+  
+  
+  
+  
+  
+  
+  </body>
+</html>
+
+
+
+
+
+
+
